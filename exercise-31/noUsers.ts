65bddd2eacffc5ae18350c1a185cae302ef39786
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:15:37 -0700

    Add files via upload

---
+++ exercise-31/noUsers.ts
@@ -0,0 +1,15 @@
+let friends:string[] = ["Usman","Shehroz","Asim","Babar"];
+
+// original list
+if (friends.length > 0) {
+    console.log("original list of friends:",friends);
+} 
+
+// remove all the friends from array
+friends = [];
+
+if (friends.length === 0) {
+    console.log("we need to find some friends");  
+} else {
+    console.log("original list of friends:",friends);
+}
