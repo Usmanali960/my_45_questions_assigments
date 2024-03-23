486470ec58d998b0b55f2a957454d6d74dde59d0
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:05:06 -0700

    Add files via upload

---
+++ exercise-20/array.js
@@ -0,0 +1,8 @@
+"use strict";
+let countries = ["pakistan", "turkey", "yemen"];
+let cities = [];
+cities.push("karachi");
+cities.push("intanbol");
+cities.push("sanaa");
+console.log(countries);
+console.log(cities);
---
+++ exercise-20/array.ts
@@ -0,0 +1,14 @@
+
+
+let countries:string[] = ["pakistan","turkey","yemen"]
+
+let cities:string[] = [];
+
+cities.push("karachi");
+cities.push("intanbol");
+cities.push("sanaa");
+
+
+console.log(countries);
+
+console.log(cities);
