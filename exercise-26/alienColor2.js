cce88565422d5bca0c1f7c76a72b143f3f20c8fd
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:09:58 -0700

    Add files via upload

---
+++ exercise-26/alienColor2.js
@@ -0,0 +1,8 @@
+"use strict";
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("Congrats you shoot the alien and you earned '5' points");
+}
+else {
+    console.log("Oh you missed the alien and you earned '3' points");
+}
---
+++ exercise-26/alienColor2.ts
@@ -0,0 +1,9 @@
+let alienColor:string = "green";
+
+if (alienColor === "green") {
+    console.log("Congrats you shoot the alien and you earned '5' points");
+    
+} else {
+    console.log("Oh you missed the alien and you earned '3' points");
+    
+}
