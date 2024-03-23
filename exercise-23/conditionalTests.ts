ab1f1afb51c316ecc796bf0dc5f1d97ca9ad1d58
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:07:37 -0700

    Add files via upload

---
+++ exercise-23/conditionalTests.ts
@@ -0,0 +1,18 @@
+let car:string = "subharu";
+
+// True cases
+console.log(car === "subharu");
+console.log(car !== "civic");
+console.log(car.endsWith("u"));
+console.log(car.startsWith("s"));
+console.log(car == "subharu");
+
+// False cases
+console.log(car !== "subharu");
+console.log(car === "civic");
+console.log(car.endsWith("c"));
+console.log(car.startsWith("u"));
+console.log(car != "subharu");
+
+
+
