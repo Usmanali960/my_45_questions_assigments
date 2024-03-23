8218ab6a328fe57ae896cfb22e37d8dddada5e90
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 05:54:42 -0700

    Add files via upload

---
+++ exercise-12/Greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+let friends = ["Usman", "Shehroz", "Asim", "Babar"];
+let message = "you are my best friend";
+for (let i = 0; i < friends.length; i++) {
+    const element = friends[i];
+    console.log(`${element} ${message} `);
+}
---
+++ exercise-12/Greetings.ts
@@ -0,0 +1,8 @@
+let friends:string[] = ["Usman","Shehroz","Asim","Babar"];
+let message:string = "you are my best friend";
+
+
+for (let i = 0; i < friends.length; i++) {
+    const element = friends[i];
+    console.log(`${element} ${message} `)
+}
