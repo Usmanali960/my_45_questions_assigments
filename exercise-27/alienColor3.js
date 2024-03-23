dc861b03cab01bd5cd5f17039288b0f224955992
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:10:51 -0700

    Add files via upload

---
+++ exercise-27/alienColor3.js
@@ -0,0 +1,34 @@
+"use strict";
+// first program
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("you shoot the alien and you got the '5' points");
+}
+else if (alienColor === "yellow") {
+    console.log("you shoot the alien and you got the '10' points");
+}
+else {
+    console.log("you shoot the alien and you got the '5' points");
+}
+// second program
+let alienColor2 = "yellow";
+if (alienColor === "yellow") {
+    console.log("you shoot the alien and you got the '10' points");
+}
+else if (alienColor === "green") {
+    console.log("you shoot the alien and you got the '10' points");
+}
+else {
+    console.log("you shoot the alien and you got the '5' points");
+}
+// third program
+let alienColor3 = "red";
+if (alienColor === "red") {
+    console.log("you shoot the alien and you got the '15' points");
+}
+else if (alienColor === "green") {
+    console.log("you shoot the alien and you got the '10' points");
+}
+else {
+    console.log("you shoot the alien and you got the '5' points");
+}
---
+++ exercise-27/alienColor3.ts
@@ -0,0 +1,38 @@
+// first program
+let alienColor:string = "green";
+
+if (alienColor === "green") {
+    console.log("you shoot the alien and you got the '5' points");
+    
+}else if(alienColor === "yellow"){
+    console.log("you shoot the alien and you got the '10' points");
+    
+} else {
+    console.log("you shoot the alien and you got the '5' points");
+}
+
+// second program
+let alienColor2:string = "yellow";
+
+if (alienColor === "yellow") {
+    console.log("you shoot the alien and you got the '10' points");
+    
+}else if(alienColor === "green"){
+    console.log("you shoot the alien and you got the '10' points");
+    
+} else {
+    console.log("you shoot the alien and you got the '5' points");
+}
+
+// third program
+let alienColor3:string = "red";
+
+if (alienColor === "red") {
+    console.log("you shoot the alien and you got the '15' points");
+    
+}else if(alienColor === "green"){
+    console.log("you shoot the alien and you got the '10' points");
+    
+} else {
+    console.log("you shoot the alien and you got the '5' points");
+}
