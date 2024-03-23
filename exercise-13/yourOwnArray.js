7b7a854887e98e1f1cbcd511dfc19025c398fec0
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 05:55:36 -0700

    Add files via upload

---
+++ exercise-13/yourOwnArray.js
@@ -0,0 +1,6 @@
+"use strict";
+let favoriteTransfortation = ["motorCycle", "car", "aeroplane", "train"];
+let message = "I would like to own a";
+favoriteTransfortation.forEach(elem => {
+    console.log(`${message} ${elem}`);
+});
---
+++ exercise-13/yourOwnArray.ts
@@ -0,0 +1,5 @@
+let favoriteTransfortation:string[] = ["motorCycle","car","aeroplane","train"];
+let message:string = "I would like to own a";
+favoriteTransfortation.forEach(elem =>{
+    console.log(`${message} ${elem}`)
+})
