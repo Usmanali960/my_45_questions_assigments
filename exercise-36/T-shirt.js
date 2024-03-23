b1cdc3fdc4de1933d5547bd82bf0207959eef981
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:22:16 -0700

    Add files via upload

---
+++ exercise-36/T-shirt.js
@@ -0,0 +1,5 @@
+"use strict";
+function Tshirt(size, message) {
+    console.log(`give me the sirt ${size} size.and the message on my T-shirt is ${message}`);
+}
+Tshirt(50, "I love typescript");
---
+++ exercise-36/T-shirt.ts
@@ -0,0 +1,5 @@
+function Tshirt(size:number,message:string) {
+    console.log(`give me the sirt ${size} size.and the message on my T-shirt is ${message}`);
+    
+}
+Tshirt(50,"I love typescript")
