32903330ec2ce30f7e8e657d40d263212d2d7f97
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:05:49 -0700

    Add files via upload

---
+++ exercise-21/object.js
@@ -0,0 +1,8 @@
+"use strict";
+let person = {
+    isName: "Usman",
+    age: 17.5,
+    isStudent: true,
+    nationality: "pakistani"
+};
+console.log(person);
---
+++ exercise-21/object.ts
@@ -0,0 +1,15 @@
+interface person{
+    isName : string,
+    age : number,
+    isStudent : boolean,
+    nationality : string
+}
+
+
+let person:person = {
+    isName : "Usman",
+    age : 17.5,
+    isStudent : true,
+    nationality : "pakistani"
+}
+console.log(person)
