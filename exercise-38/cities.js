160e901190e8456584ffe6b2105f2c2022f6267c
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 07:51:55 -0700

    Add files via upload

---
+++ exercise-38/cities.js
@@ -0,0 +1,7 @@
+"use strict";
+function describeCity(city, country = "pakistan") {
+    console.log(`${city} is in ${country}`);
+}
+describeCity("karachi");
+describeCity("lahore");
+describeCity("newYork", "USA");
---
+++ exercise-38/cities.ts
@@ -0,0 +1,6 @@
+function describeCity(city:string,country:string = "pakistan") {
+    console.log(`${city} is in ${country}`);
+}
+describeCity("karachi");
+describeCity("lahore");
+describeCity("newYork","USA")
