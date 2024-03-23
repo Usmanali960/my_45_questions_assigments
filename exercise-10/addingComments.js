c4ab912343abba681f624951a84d12976b6ed21c
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 05:49:28 -0700

    Add files via upload

---
+++ exercise-10/addingComments.js
@@ -0,0 +1,5 @@
+"use strict";
+// my name is Usman
+// current date: tue,19,24
+// this program multiply two numbers
+console.log(10 * 20);
---
+++ exercise-10/addingComments.ts
@@ -0,0 +1,4 @@
+// my name is Usman
+// current date: tue,19,24
+// this program multiply two numbers
+console.log(10 * 20)
