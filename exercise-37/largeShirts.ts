202fb796073b932c4f5222c903097810f14cfcb0
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:25:20 -0700

    Add files via upload

---
+++ exercise-37/largeShirts.ts
@@ -0,0 +1,7 @@
+function largeShirts(size:string = "large",message:string = "I love typescript") {
+    console.log(`creating a ${size} shirt with the text of ${message}`);
+    
+}
+largeShirts();
+largeShirts("medium");
+largeShirts("small","I love python")
