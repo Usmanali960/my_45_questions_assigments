d020d386380ad87d08858cee32c125f29d7c3f34
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 08:21:21 -0700

    Add files via upload

---
+++ exercise-41/magicians.js
@@ -0,0 +1,9 @@
+"use strict";
+let magicians = ["harryHodini", "davidBlaine", "dougHenning"];
+function showMagicians() {
+    let topMagicians = magicians;
+    for (let magic of magicians) {
+        console.log(magic);
+    }
+}
+showMagicians();
---
+++ exercise-41/magicians.ts
@@ -0,0 +1,9 @@
+let magicians:string[] = ["harryHodini","davidBlaine","dougHenning"];
+function showMagicians() {
+    let topMagicians = magicians;
+    for (let magic of magicians) {
+        console.log(magic);
+        
+    }
+}
+showMagicians();
