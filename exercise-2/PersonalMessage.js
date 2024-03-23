bd3517995f0bd41ef756431a579f518922356042
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 04:29:35 -0700

    Add files via upload

---
+++ exercise-2/PersonalMessage.js
@@ -0,0 +1,3 @@
+"use strict";
+let UserName = "Usman";
+console.log(`hello ${UserName} would you like to learn some python today`);
---
+++ exercise-2/PersonalMessage.ts
@@ -0,0 +1,2 @@
+let UserName:string = "Usman";
+console.log(`hello ${UserName} would you like to learn some python today`)
