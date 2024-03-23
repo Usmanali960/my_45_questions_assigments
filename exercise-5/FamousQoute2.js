3d977305090c1d59d382447ee69355b42014cea2
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 04:45:15 -0700

    Add files via upload

---
+++ exercise-5/FamousQoute2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famousPerson = "Albert Einstien";
+let message = "A person who never made a mistake naver tried anything new!";
+console.log(`${famousPerson} Once said ${message}`);
---
+++ exercise-5/FamousQoute2.ts
@@ -0,0 +1,3 @@
+let famousPerson:string = "Albert Einstien";
+let message:string = "A person who never made a mistake naver tried anything new!";
+console.log(`${famousPerson} Once said ${message}`)
