2424d0073212e2af1c2412480ba509a2e2c31da8
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 08:18:04 -0700

    Add files via upload

---
+++ exercise-39/cityNames.js
@@ -0,0 +1,7 @@
+"use strict";
+function cityCountry(city, country) {
+    return `${city} is in ${country}`;
+}
+console.log(cityCountry("bombay", "india"));
+console.log(cityCountry("karachi", "pakistan"));
+console.log(cityCountry("istanbol", "turkey"));
---
+++ exercise-39/cityNames.ts
@@ -0,0 +1,7 @@
+function cityCountry(city:string,country:string) {
+    return `${city} is in ${country}`;
+    
+}
+console.log(cityCountry("bombay","india"));
+console.log(cityCountry("karachi","pakistan"));
+console.log(cityCountry("istanbol","turkey"));
