199956fef89357cdf593483e64341f4be32ad85c
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:08:20 -0700

    Add files via upload

---
+++ exercise-24/moreConditionalTests.js
@@ -0,0 +1,19 @@
+"use strict";
+// Equality test or Inequality tset with strings
+console.log("apple" === "apple");
+console.log("apple" != "apple");
+// lower case function
+console.log("USMAN".toLowerCase() === "usman");
+//greater than or less than
+console.log(5 > 2);
+console.log(5 < 2);
+//greater than or equal to less than or equal to
+console.log(5 >= 2);
+console.log(5 <= 2);
+// test with and operator and test with or operator
+console.log(5 === 5 && 10 === 10);
+console.log(5 === 5 || true);
+// test item is in array or test item is not in array
+let arr = ["apple"];
+arr.includes("apple");
+arr.includes("orange");
