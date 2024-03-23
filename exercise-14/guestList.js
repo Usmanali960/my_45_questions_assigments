b99b859a20e9c8f6581b1242ade217c52d5fa98e
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 05:56:42 -0700

    Add files via upload

---
+++ exercise-14/guestList.js
@@ -0,0 +1,6 @@
+"use strict";
+let dinnerInvite = ["Usman", "Shehroz", "Asim"];
+for (let i = 0; i < dinnerInvite.length; i++) {
+    const element = dinnerInvite[i];
+    console.log(`Dr. ${dinnerInvite[i]}!\nI invite you in my tonight pary.please come in my party.`);
+}
