0cdda3b22ebbbe40ad39169074c828132ba4687c
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 06:13:36 -0700

    Add files via upload

---
+++ exercise-29/favoriteFruit.ts
@@ -0,0 +1,18 @@
+let favoriteFruits:string[] = ["banana","apple","mango","nashpati","angoor"];
+
+if (favoriteFruits.includes("banana")) {
+    console.log(`i really like banana`);
+    
+}if (favoriteFruits.includes("apple")) {
+    console.log(`i really like apple`);
+    
+}if (favoriteFruits.includes("mango")) {
+    console.log(`i really like mango`);
+    
+}if (favoriteFruits.includes("nashpati")) {
+    console.log(`i really like nashpati`);
+    
+}if (favoriteFruits.includes("angoor")) {
+    console.log(`i really like angoor`);
+    
+}
