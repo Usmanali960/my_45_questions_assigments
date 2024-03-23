eb2bd14b8c11731548e55f15c14dbb260b0a94b0
Usman_developer <150580509+Usmanali960@users.noreply.github.com>
2024-03-23 05:53:45 -0700

    Add files via upload

---
+++ exercise-11/Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let myFriend = ["Usman", "Shehroz", "Asim", "Babar"];
+for (let i = 0; i < myFriend.length; i++) {
+    const element = myFriend[i];
+    console.log(element);
+}
---
+++ exercise-11/Names.ts
@@ -0,0 +1,6 @@
+let myFriend:string[] = ["Usman","Shehroz","Asim","Babar"]
+
+for (let i = 0; i < myFriend.length; i++) {
+    const element = myFriend[i];
+    console.log(element)
+}
